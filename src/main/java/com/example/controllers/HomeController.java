800795bed4272dc82c63838196a20ef1122333a9
Santosh Masiwal <santoshmasiwal55@gmail.com>
2020-08-16 00:27:11 +0530

    First commit

---
+++ src/main/java/com/example/controllers/HomeController.java
@@ -0,0 +1,34 @@
+package com.example.controllers;
+
+import java.awt.Taskbar.State;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.models.LocationStats;
+import com.example.service.CoronavirusDataService;
+
+@Controller
+public class HomeController {
+  
+  @Autowired
+  CoronavirusDataService coronavirusDataService;
+  
+  @GetMapping("/")
+  public String home(Model model)
+  {
+    List<LocationStats> allStats = coronavirusDataService.getAllStats();
+    int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+    
+    int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+    
+    model.addAttribute("locationStats", allStats);
+    model.addAttribute("totalReportedCases", totalReportedCases);
+    model.addAttribute("totalNewCases", totalNewCases);
+    return "home";
+  }
+
+}
